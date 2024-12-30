63678b0a6d4005297bf4fc7c814a56caad266beb
mian-jimeru <Churraca_1>
2024-12-30 12:43:38 +0100

    inicial comit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,13 @@ import './App.css';
 
 
 function App() {
+  
+  const initialTasks = [
+    {id: 0, text: 'Visitar el Museo Kafka', done: true},
+    {id: 1, text: 'Ver espectáculo de títeres', done: false},
+    {id: 2, text: 'Foto del muro de Lennon', done: false},
+  ];
+
   return (
     <div className="App">
       <AddTask />
